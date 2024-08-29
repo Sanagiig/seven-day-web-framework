9566a4dbcd59c79cadbc9cc44aa7b24aaf1f7cc0
Sanagiig <laiwenjunhhh@163.com>
2024-08-29 14:36:12 +0800

    update

--- geeRPC2/xclient_discovery.go
+++ geeRPC2/xclient_discovery.go
@@ -1,3 +1,11 @@
+/*
+ * @Author: Sanagiig laiwenjunhhh@163.com
+ * @Date: 2024-08-29 10:41:45
+ * @LastEditors: Sanagiig laiwenjunhhh@163.com
+ * @LastEditTime: 2024-08-29 14:36:09
+ * @FilePath: \seven-day-web-framework\geeRPC2\xclient_discovery.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 package geerpc
 
 import (
